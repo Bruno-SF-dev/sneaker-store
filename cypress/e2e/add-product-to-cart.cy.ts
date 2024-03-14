5d56a690839cb56bafece6b366a17471c239eefb
Bruno Santos <brunosantosfarias2@gmail.com>
2024-03-13 23:50:59 -0300

    feat: implements test to add product to cart

---
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -0,0 +1,38 @@
+describe('add product to cart', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('deve ser possível navegar para a página do produto e adicionar o produto ao carrinho', () => {
+    cy.get('a[href^="/product"]').first().click();
+
+    cy.location('pathname').should('include', '/product');
+
+    cy.contains('Adicionar ao carrinho').click();
+
+    cy.contains('Carrinho (1)').should('exist');
+  });
+
+  it('não deve ser possível adicionar o mesmo produto no carrinho', () => {
+    cy.get('a[href^="/product"]').first().click();
+
+    cy.location('pathname').should('include', '/product');
+
+    cy.contains('Adicionar ao carrinho').click();
+    cy.contains('Adicionar ao carrinho').click();
+
+    cy.contains('Carrinho (1)').should('exist');
+  });
+
+  it('deve ser ser possível buscar um produto e adicioná-lo ao carrinho', () => {
+    cy.get('input[name="q"]').type('moletom').parent('form').submit();
+
+    cy.get('a[href^="/product"]').first().click();
+
+    cy.location('pathname').should('include', '/product');
+
+    cy.contains('Adicionar ao carrinho').click();
+
+    cy.contains('Carrinho (1)').should('exist');
+  });
+});
