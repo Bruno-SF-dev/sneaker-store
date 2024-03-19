562c9a72b614cadf319822f2abec89e2ba54a6f9
Bruno Santos <brunosantosfarias2@gmail.com>
2024-03-19 19:07:19 -0300

    fix: body relative

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt" className={`${inter.variable} ${leagueGothic.variable}`}>
-      <body className="bg-zinc-950 text-zinc-50 antialiased">
+      <body className="relative bg-zinc-950 text-zinc-50 antialiased">
         <div className="absolute inset-0 rounded-md bg-gradient-to-r to-sky-500 from-transparent opacity-[0.05]"></div>
         <div className="relative">{children}</div>
       </body>
