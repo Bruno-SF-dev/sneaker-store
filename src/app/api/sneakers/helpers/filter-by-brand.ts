0a8a01be0f38a4e5d5f190a56a2c0ecb32910650
Bruno Santos <brunosantosfarias2@gmail.com>
2024-03-25 20:38:47 -0300

    chore: extract api

--- src/app/api/sneakers/helpers/filter-by-brand.ts
+++
@@ -1,9 +0,0 @@
-import { Sneaker } from '@/api/data/types/sneakers';
-
-export const filterByBrand = (data: Sneaker[], brandsToFilter: string[]) => {
-  const filteredData = data.filter((snk) =>
-    brandsToFilter.includes(snk.brand_name)
-  );
-
-  return filteredData;
-};
--- src/app/api/sneakers/helpers/filter-by-gender.ts
+++
@@ -1,9 +0,0 @@
-import { Sneaker } from '@/api/data/types/sneakers';
-
-export const filterByGender = (data: Sneaker[], genresToFilter: string[]) => {
-  const filteredData = data.filter((snk) =>
-    snk.gender.some((cat) => genresToFilter.includes(cat))
-  );
-
-  return filteredData;
-};
